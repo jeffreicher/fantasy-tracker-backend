c84240b5525ead50513fcc8180fee7b114056320
Jeffrei Cher <jeffreicher@yahoo.com>
2019-02-07 16:41:25 -0800

    strange that the supertest doesnt have a query date response but live does

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const scoreboard = require('../routes/scoreboard')
 router.get('/scoreboard', async function(req, res) {
   // let querystring = window.location.search.substring(1)
   // console.log(querystring)
+  console.log(req)
   console.log(req.query)
   let response = await scoreboard(req.query.date)
   // return response
